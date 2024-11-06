59a8840ef5896ca8788b8a8c96a6229b5811673d
kinghansu <whtpq08@naver.com>
2024-11-06 11:26:26 +0900

    7주차 일단 완료

--- demo/src/main/java/com/example/demo/controller/BlogController.java
+++ demo/src/main/java/com/example/demo/controller/BlogController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import com.example.demo.model.domain.Article;
+import com.example.demo.model.domain.Board;
 import com.example.demo.model.service.AddArticleRequest;
 import com.example.demo.model.service.BlogService;
 import lombok.RequiredArgsConstructor;
@@ -17,33 +18,46 @@ public class BlogController {
 
     private final BlogService blogService;
 
-    @GetMapping("/article_list")
+   /*  @GetMapping("/article_list")
     public String getArticles(Model model) {
         List<Article> articles = blogService.findAll();
         model.addAttribute("articles", articles);
         return "article_list";  // article_list.html 페이지로 이동
-    }
+    }*/
 
     // POST 요청으로 게시글 추가
-    @PostMapping("/api/articles")
+    /*@PostMapping("/api/articles")
     public String addArticle(@ModelAttribute AddArticleRequest request) {
         blogService.save(request);  // 게시글 저장
         return "redirect:/article_list";  // 게시글 추가 후 목록 페이지로 리다이렉트
-    }
+    }*/
 
-    @PutMapping("/api/article_edit/{id}")
+    /*@PutMapping("/api/article_edit/{id}")
     public String updateArticle(@PathVariable Long id, @ModelAttribute AddArticleRequest request) {
         blogService.update(id, request);
         return "redirect:/article_list"; // 글 수정 후 목록 페이지로 리다이렉트
-    }
+    }*/
 
-    @DeleteMapping("/api/article_delete/{id}")
+    /*@PutMapping("/api/board_edit/{id}")
+    public String updateArticle(@PathVariable Long id, @ModelAttribute AddArticleRequest request) {
+        blogService.update(id, request);
+        return "redirect:/board_list"; // 글 수정 후 목록 페이지로 리다이렉트
+    }*/
+
+    /*@DeleteMapping("/api/article_delete/{id}")
     public String deleteArticle(@PathVariable Long id) {
         blogService.delete(id);
         return "redirect:/article_list";  // 삭제 후 목록 페이지로 리다이렉트
-    }
+    }*/
 
-    @GetMapping("/article_edit/{id}")
+    /*@DeleteMapping("/api/board_delete/{id}")
+    public String deleteArticle(@PathVariable Long id) {
+        blogService.delete(id);
+        return "redirect:/board_list";  // 삭제 후 목록 페이지로 리다이렉트
+    }*/
+
+
+    /*@GetMapping("/article_edit/{id}")
     public String articleEdit(Model model, @PathVariable Long id) {
         Optional<Article> article = blogService.findById(id);
         if (article.isPresent()) {
@@ -52,5 +66,27 @@ public class BlogController {
             return "error_page/article_error"; // 오류 페이지로 리다이렉트
         }
         return "article_edit"; // article_edit.html 페이지 반환
+    }*/
+
+
+    @GetMapping("/board_list") // 새로운 게시판 링크 지정
+    public String board_list(Model model) {
+     List<Board> list = blogService.findAll(); // 게시판 전체 리스트
+    model.addAttribute("boards", list); // 모델에 추가
+    return "board_list"; // .HTML 연결
     }
+
+    @GetMapping("/board_view/{id}") // 게시판 링크 지정
+    public String board_view(Model model, @PathVariable Long id) {
+     Optional<Board> list = blogService.findById(id); // 선택한 게시판 글
+
+     if (list.isPresent()) {
+        model.addAttribute("boards", list.get()); // 존재할 경우 실제 Article 객체를 모델에 추가
+       } else {
+        // 처리할 로직 추가 (예: 오류 페이지로 리다이렉트, 예외 처리 등)
+        return "/error_page/article_error"; // 오류 처리 페이지로 연결
+       }
+        return "board_view"; // .HTML 연결
+    
+}
 }
--- demo/src/main/java/com/example/demo/model/service/BlogService.java
+++ demo/src/main/java/com/example/demo/model/service/BlogService.java
@@ -7,23 +7,43 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.model.domain.Article;
-import com.example.demo.model.repository.BlogRepository;
+import com.example.demo.model.domain.Board;
+import com.example.demo.model.repository.BoardRepository;
+
 import lombok.RequiredArgsConstructor;
+
+
+
 
 @Service
 @RequiredArgsConstructor // 생성자 자동 생성
 public class BlogService {
 
     @Autowired
-    private final BlogRepository blogRepository;
+    //private final BlogRepository blogRepository;
+     private final BoardRepository blogRepository; // 리포지토리 선언
+
+
+      // 게시판 전체 목록 조회
+    //public List<Article> findAll() {
+      //  return blogRepository.findAll();
+    //}
 
     // 게시판 특정 글 조회
-    public Optional<Article> findById(Long id) {
-        return blogRepository.findById(id);
+    //public Optional<Article> findById(Long id) {
+        //return blogRepository.findById(id);
+    //}
+
+    public List<Board> findAll() { // 게시판 전체 목록 조회
+    return blogRepository.findAll();
+    }
+
+    public Optional<Board> findById(Long id) { // 게시판 특정 글 조회
+     return blogRepository.findById(id);
     }
 
     // 게시글 수정
-    public void update(Long id, AddArticleRequest request) {
+    /*public void update(Long id, AddArticleRequest request) {
         Optional<Article> optionalArticle = blogRepository.findById(id); // 단일글조회
        optionalArticle.ifPresent(article -> { //값이있으면
        article.update(request.getTitle(), request.getContent()); // 값을수정
@@ -31,10 +51,6 @@ public class BlogService {
        });
         }
 
-    // 게시판 전체 목록 조회
-    public List<Article> findAll() {
-        return blogRepository.findAll();
-    }
 
     // 게시글 저장
     public Article save(AddArticleRequest request) {
@@ -42,5 +58,5 @@ public class BlogService {
     }
     public void delete(Long id) {
         blogRepository.deleteById(id);
-        }
+        }*/
 }
